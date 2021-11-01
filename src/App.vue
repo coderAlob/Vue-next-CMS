bdf4460d9d5acb3391b6d8b905355f5c777657a2
coderAlob <1649327177@qq.com>
2021-11-01 16:56:00 +0800

    build: project init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h2>{{ $store.state.name }}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from "vue"
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+  setup() {
+    return {}
+  }
+})
 </script>
 
-<style lang="less">
+<style lang="css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require("path")
+
+module.exports = {
+  // chainWebpack: config => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
+        views: "@/views"
+      }
+    }
+  }
+}
